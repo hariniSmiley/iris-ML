6cc284ec53c3431149539640c8063056c1741201
harini_smiley15 <harinismiley2006@gmail.com>
2025-07-09 12:20:30 +0530

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,1 +1,16 @@
+# predict.py
+import joblib
+import numpy as np
 
+# Load model and label encoder
+model = joblib.load('iris_model.pkl')
+le = joblib.load('label_encoder.pkl')
+
+# Example input: [sepal_length, sepal_width, petal_length, petal_width]
+input_data = np.array([[5.1, 3.5, 1.4, 0.2]])
+
+# Predict
+prediction = model.predict(input_data)
+predicted_species = le.inverse_transform(prediction)
+
+print(f"Predicted Iris species: {predicted_species[0]}")
